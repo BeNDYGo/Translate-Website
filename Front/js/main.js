27b96593cf32fc73fc7bc3691b37704dcf2d7fbc
BeNDYGo <palexascha@gmail.com>
2026-03-01 16:19:58 +0300

    -

--- Front/js/main.js
+++ Front/js/main.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     const input = document.getElementById('input')
     const translateButton = document.getElementById('Translate-Button')
     const output = document.getElementById('output')
+    const addButton = document.getElementById('Add-Button')
+    const wordsLearning = document.querySelector('.words-learning')
 
     // Функция измерения пинга
     async function measurePing() {
@@ -27,6 +29,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     
     translateButton.addEventListener('click', async () => {
         const text = input.value
+        const buttonContainer = document.querySelector('.button-container')
+        addButton.style.display = 'none'
+        buttonContainer.classList.remove('has-add')
         output.innerHTML = 'Loading...'
         if (text === '') return
         
@@ -46,6 +51,37 @@ document.addEventListener('DOMContentLoaded', async () => {
                 <div><strong>Google:</strong> ${data.google || 'нет перевода'}</div>
                 <div><strong>Wooordhunt:</strong> ${data.wooordhunt || 'нет перевода'}</div>
             `
+        addButton.style.display = 'block'
+        buttonContainer.classList.add('has-add')
+    })
+
+    function addWordToContainer(word) {
+        const newWord = document.createElement('li')
+        newWord.textContent = word
+        newWord.addEventListener('dblclick', function() {
+            this.remove()
+            let words = JSON.parse(localStorage.getItem('learningWords')) || []
+            words = words.filter(w => w !== word)
+            localStorage.setItem('learningWords', JSON.stringify(words))
+        })
+        wordsLearning.querySelector('.words-container').prepend(newWord)
+    }
+
+    addButton.addEventListener('click', () => {
+        wordsLearning.style.display = 'block'
+        addWordToContainer(input.value)
+        
+        const currentWords = JSON.parse(localStorage.getItem('learningWords')) || []
+        currentWords.push(input.value)
+        localStorage.setItem('learningWords', JSON.stringify(currentWords))
+    })
+
+    // Загрузка сохраненных слов
+    const words = JSON.parse(localStorage.getItem('learningWords')) || []
+    const wordsContainer = wordsLearning.querySelector('.words-container')
+    wordsContainer.innerHTML = ''
+    words.forEach(word => {
+        addWordToContainer(word)
     })
 
     measurePing()
